7848fa5d1b8d6decce9ee0393a91576e70217e02
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-01-05 17:15:38 +0900

    20230105PM1715
    getTodos
    
    20230105PM1715

--- src/components/route/mainApp/TodoApp.tsx
+++ src/components/route/mainApp/TodoApp.tsx
@@ -1,9 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from '../../../stylesheets/route/mainApp/TodoApp.module.css';
 import TodoElement from "../../todo/TodoElement";
+import {getUserInfomation, setAuthToken, setUserInformation} from "../../../modules/auth/authValidation";
+import PublicMessageBox from "../../public/PublicMessageBox";
 
 function TodoApp(){
     const [todoInputVal, setTodoInputVal] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    const getTodoList = async() => {
+        let userInfo = await getUserInfomation();
+        if(userInfo){
+            fetch('http://localhost:8080/todos', {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Authorization': userInfo['token'],
+                },
+            }).then((response) => {
+                console.log('response ', response);
+                if(response.ok){
+                    return response.json();
+                }
+                throw new Error('Network response was not ok.');
+            }).then((data) => {
+                console.log('성공 ', data);
+                if(data){
+                    setTodos(data.data);
+                    console.log('todos ', todos);
+                }
+            }).catch((error) => {
+                console.error('실패 : ', error);
+                PublicMessageBox('할 일을 불러오지 못했어요.');
+            })
+        }
+    }
+
+    useEffect(() => {
+        getTodoList();
+    }, [])
 
     const todoInputOnChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTodoInputVal(event.target.value);
@@ -15,7 +50,10 @@ function TodoApp(){
                 <span><b>할 일</b>을 관리해보세요!</span>
             </div>
             <div className={styles.todo_main_body}>
-                <TodoElement />
+                { Array.isArray(todos) ? todos.map((todo, index) => {
+                    console.log('todo ', todo);
+                    return <TodoElement />
+                }) : null}
             </div>
             <div className={styles.todo_main_create_container}>
                 <div className={styles.todo_main_create_div}>
