b215c86e169a3be6bae6dc6fbd53fecbb1355988
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-01-05 11:31:12 +0900

    20230105AM1130
    login, signup page input error
    
    20230105AM1130

--- src/components/auth/AuthInputComponent.tsx
+++ src/components/auth/AuthInputComponent.tsx
@@ -1,6 +1,6 @@
 import React, {useState, forwardRef} from 'react';
 import styles from '../../stylesheets/components/auth/AuthInputComponent.module.css';
-import { emailInputValidation } from '../../modules/auth/authValidation';
+import {emailInputValidation, passwordInputValidation} from '../../modules/auth/authValidation';
 
 type AuthInputComponentType = {
     inputType: string,
@@ -10,35 +10,50 @@ type AuthInputComponentType = {
 function AuthInputComponent({inputType, inputLabel}: AuthInputComponentType, ref: React.ForwardedRef<HTMLInputElement>){
     const [inputVal, setInputVal] = useState('');
     const [isError, setIsError] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('');
 
-    let errorAddHtml = ``;
+    let errorAddHtml = `<div style="position: absolute; bottom: -20px;display: flex; width: 100%"><span style="font-size: 0.8rem; color: red;">${errorMsg}</span></div>`;
+
     const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         if(event){
             if(inputType === 'email'){
                 let validationResult = emailInputValidation(inputVal);
-                console.log('validationResult ', validationResult);
                 if(validationResult.isValid){
-                    errorAddHtml = '';
+                    setErrorMsg('');
                     setIsError(false);
                 } else {
-                    errorAddHtml = `<div className={styles.error_container}><span>{validationResult.message}</span></div>`
+                    setErrorMsg(validationResult.message);
                     setIsError(true);
-                    console.log('asda');
                 }
             } else if(inputType === 'password'){
+                let validationResult = passwordInputValidation(inputVal);
 
+                if(validationResult.isValid){
+                    setErrorMsg('');
+                    setIsError(false);
+                } else {
+                    setErrorMsg(validationResult.message);
+                    setIsError(true);
+                }
             }
 
+            //set input value
             setInputVal(event.target.value);
+        }
+    }
+
+    const onBlurHandler = (event: React.FocusEvent<HTMLInputElement>) => {
+        if(event && event.target.value === ''){
+            setErrorMsg('');
         }
     }
 
     return (
         <div className={styles.auth_input_component_root}>
-            <input ref={ref} className={styles.auth_input} type={inputType} onChange={onChangeHandler} value={inputVal} required/>
+            <input ref={ref} className={styles.auth_input} type={inputType === 'email' ? 'text' : 'password'} onChange={onChangeHandler} onBlur={onBlurHandler} value={inputVal} required/>
             <span className={styles.auth_txt_area}></span>
             <label className={styles.auth_input_header_label}>{inputLabel}</label>
-            {isError ? errorAddHtml : null}
+            {isError ? <div dangerouslySetInnerHTML={{__html: errorAddHtml}}></div> : null}
         </div>
     );
 }
--- src/modules/auth/authValidation.tsx
+++ src/modules/auth/authValidation.tsx
@@ -20,17 +20,24 @@ export const emailInputValidation = (value: string) => {
 
     return validationResult
 }
-//
-// export const passwordInputValidation = (value: string) => {
-//     let validationResult = {
-//         isValid: false,
-//         message: '',
-//     }
-//
-//     if(passwordInputRef && passwordInputRef.current){
-//
-//
-//     }
-//
-//     return validationResult
-// }
+
+export const passwordInputValidation = (value: string) => {
+    let validationResult = {
+        isValid: false,
+        message: '',
+    }
+    let regex = /(?=.*\d)(?=.*[a-z]).{8,}/; // 영어 소문자, 숫자 포함 8자 이상의 비밀번호
+    if(value){
+        let regexResult = regex.test(value);
+
+        if (regexResult){
+            validationResult.isValid = true;
+            validationResult.message = '';
+        } else {
+            validationResult.isValid = false;
+            validationResult.message = '올바른 비밀번호 형식이 아닙니다.';
+        }
+    }
+
+    return validationResult
+}
