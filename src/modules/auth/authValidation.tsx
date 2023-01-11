192a6dda17ec60fb147d442d0e927cda490cd61d
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-01-11 13:45:14 +0900

    refactor. 20230111
    authValidation 모듈 전체 코드 리팩토링
    
     - emailInputValidation
     - passwordInputValidation
     - getAuthToken
     - setUserInformation

--- src/modules/auth/authValidation.tsx
+++ src/modules/auth/authValidation.tsx
@@ -1,87 +1,97 @@
 import React from "react";
 
 export const emailInputValidation = (value: string) => {
-    let validationResult = {
+    let regex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+    if(value){
+        let regexResult = regex.test(value);
+
+        if (regexResult){
+            return {
+                isValid: true,
+                message: '',
+            }
+        } else {
+            return {
+                isValid: false,
+                message: '올바른 이메일 형식이 아닙니다.',
+            }
+        }
+    }
+
+    return {
         isValid: false,
         message: '올바른 이메일 형식이 아닙니다.',
     }
-    let regex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-    if(value){
-        let regexResult = regex.test(value);
-
-        if (regexResult){
-            validationResult.isValid = true;
-            validationResult.message = '';
-        } else {
-            validationResult.isValid = false;
-            validationResult.message = '올바른 이메일 형식이 아닙니다.';
-        }
-    }
-
-    return validationResult
 }
 
 export const passwordInputValidation = (value: string) => {
-    let validationResult = {
-        isValid: false,
-        message: '',
-    }
     let regex = /(?=.*\d)(?=.*[a-z]).{8,}/; // 영어 소문자, 숫자 포함 8자 이상의 비밀번호
+
     if(value){
         let regexResult = regex.test(value);
 
         if (regexResult){
-            validationResult.isValid = true;
-            validationResult.message = '';
+            return {
+                isValid: true,
+                message: '',
+            }
         } else {
-            validationResult.isValid = false;
-            validationResult.message = '올바른 비밀번호 형식이 아닙니다.';
+            return {
+                isValid: false,
+                message: '올바른 비밀번호 형식이 아닙니다.',
+            }
         }
     }
 
-    return validationResult
+    return {
+        isValid: false,
+        message: '',
+    }
 }
 
 export const getAuthToken = () => {
-    let getAuthTokenResult = {
-        isToken: false,
-    }
-
     let userAuthResult = localStorage.getItem('token');
 
     if(userAuthResult){
-        getAuthTokenResult.isToken = true;
+        return {
+            isToken: true,
+        }
     } else {
-        getAuthTokenResult.isToken = false;
+        return {
+            isToken: false,
+        }
     }
 
-    return getAuthTokenResult;
+    return {
+        isToken: false,
+    };
 }
 
 export const setAuthToken = (token: string) => {
-    let setAuthTokenResult = {
-        success: false,
-    }
-
     if(token){
         localStorage.setItem('token', token);
-        setAuthTokenResult.success = true;
+        return {
+            success: true,
+        }
     }
 
-    return setAuthTokenResult;
+    return {
+        success: false,
+    };
 }
 
 export const setUserInformation = (email: string) => {
-    let setUserInfoResult = {
-        success: false,
-    }
-
     if(email){
         localStorage.setItem('email', email);
-        setUserInfoResult.success = true;
+        return {
+            success: true,
+        };
     }
 
-    return setUserInfoResult;
+    return {
+        success: false,
+    };
 }
 
 export const getUserInformation = () => {
